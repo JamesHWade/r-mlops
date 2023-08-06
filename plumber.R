68f6063f8683e2ab64e738ed748dfe8d27b82bd7
James Wade <github@jameshwade.com>
2023-08-06 19:04:24 -0400

    add hyperparameter presentation

--- plumber.R
+++ plumber.R
@@ -13,7 +13,7 @@ if (FALSE) {
     library(workflows)
 }
 b <- board_folder(path = "pins-r")
-v <- vetiver_pin_read(b, "penguins_model", version = "20230730T192359Z-54641")
+v <- vetiver_pin_read(b, "penguins_model", version = "20230730T235625Z-54641")
 
 #* @plumber
 function(pr) {
